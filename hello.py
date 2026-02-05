bb678764387042d89dbc181a834630f5ee1695aa
djraj <dj.rajasekar@gmail.com>
2026-02-05 16:37:54 -0500

    Add movie input feature to Streamlit app

--- hello.py
+++ hello.py
@@ -2,3 +2,6 @@ import streamlit as st
 st.title("Hello, Streamlit!")
 
 st.write("Welcome to your first Streamlit app.")
+
+mov = st.text_input("What is your favorite movie?")
+st.write(f"your favourite movies is {mov}")
